4727b35c55c4c1dcd51dbafdcd5b7d2f52426edb
lvkv <velikovlukas@gmail.com>
2026-04-09 16:06:18 -0400

    libresolv-sys: use bindgen package, update cargo

--- libresolv-sys/build.rs
+++ libresolv-sys/build.rs
@@ -1,57 +1,58 @@
 use std::error::Error;
-use std::fs::{self, File};
-use std::io::Write; // File.write_all()
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use std::str;
 
 fn main() -> Result<(), Box<dyn Error>> {
     static BINDING: &str = "resolv.rs";
-    let header = format!("{}/resolv.h", std::env::var("GLIBC_INCLUDE").unwrap_or("/usr/include".into()));
-    let mut cmd;
-    let mut output;
-
-    eprintln!("Generating binding {:?} from {:?} ...\n", BINDING, header);
-    cmd = Command::new("bindgen");
-    cmd.args(["--with-derive-default", &header]);
-    output = cmd.output()?;
-    if !output.status.success() {
-        let msg = str::from_utf8(output.stderr.as_slice())?;
-        eprintln!("\"\"\"\n{}\"\"\"\n", msg);
-        panic!("{:?}", cmd);
-    }
-
-    let lints = fs::read("resolv.lints")?;
-    let mut f = File::create(BINDING)?;
-    f.write_all(lints.as_slice())?;
-    f.write_all(output.stdout.as_slice())?;
-
-    eprintln!(
-        "Checking available libresolv adapters to the generated binding {:?} ...\n",
-        BINDING
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
+    let header = format!(
+        "{}/resolv.h",
+        std::env::var("GLIBC_INCLUDE").unwrap_or_else(|_| "/usr/include".into())
     );
+
+    eprintln!("Generating binding {BINDING:?} from {header:?} ...\n");
+    let bindings = bindgen::Builder::default()
+        .header(&header)
+        .derive_default(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings.write_to_file(out_dir.join(BINDING))?;
+
+    eprintln!("Checking available libresolv adapters to the generated binding {BINDING:?} ...\n");
 
     let mut paths: Vec<PathBuf> = fs::read_dir("lib.d")?.map(|e| e.unwrap().path()).collect();
     paths.sort_unstable();
+    let mut selected_path = None;
     for path in paths.iter().rev() {
-        eprintln!("Trying {:?} ...\n", path);
-        fs::copy(path, "lib.rs")?;
-        cmd = Command::new("rustc");
+        eprintln!("Trying {:?} ...\n", path.display());
+        fs::copy(path, out_dir.join("lib.rs"))?;
+        let mut cmd = Command::new("rustc");
         cmd.args(["--emit", "dep-info=/dev/null", "lib.rs"]);
-        output = cmd.output()?;
+        cmd.current_dir(&out_dir);
+        let output = cmd.output()?;
         if output.status.success() {
             eprintln!("Success\n");
+            selected_path = Some(path.clone());
             break;
-        } else {
-            let msg = str::from_utf8(output.stderr.as_slice())?;
-            eprintln!("\"\"\"\n{}\"\"\"\n", msg);
-            fs::remove_file("lib.rs")?;
         }
+        let msg = str::from_utf8(output.stderr.as_slice())?;
+        eprintln!("\"\"\"\n{msg}\"\"\"\n");
+        fs::remove_file(out_dir.join("lib.rs"))?;
     }
 
-    if !output.status.success() {
-        panic!("None of the available adapters compiled successfully!");
-    }
+    let selected_path = selected_path.expect("No available adapters compiled successfully");
+    let adapter = fs::read_to_string(selected_path)?;
+    let generated = adapter.replace(
+        "mod resolv;",
+        r#"#[allow(dead_code, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_imports)]
+mod resolv {
+    include!(concat!(env!("OUT_DIR"), "/resolv.rs"));
+}"#,
+    );
+    fs::write(out_dir.join("lib_generated.rs"), generated)?;
 
     println!("cargo:rustc-flags=-l resolv");
 
